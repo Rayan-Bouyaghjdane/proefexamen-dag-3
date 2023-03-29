1619e4d2358d266bfe6966cc2dc74082f1ad2906
Bouyaghjdane <332766@student.mboutrecht.nl>
2023-03-29 09:00:22 +0100

    Migrations
    alle migrations met relation toegevoegd

---
+++ database/migrations/2023_03_29_072954_create_persoons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('persoons', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('TypePersoonId')->constrained('type_persoons')->onDelete('cascade');
+            $table->string('Voornaam');
+            $table->string('Tussenvoegsel')->nullable();
+            $table->string('Achternaam');
+            $table->string('Roepnaam')->nullable();
+            $table->boolean('IsVolwassen');
+            $table->boolean('IsActief');
+            $table->string('Opmerking')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('persoons');
+    }
+};
---
+++ database/migrations/2023_03_29_073033_create_reserverings_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reserverings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('PersoonId')->constrained('persoons')->onDelete('cascade');
+            $table->foreignId('OpeningstijdId')->constrained('openingstijds')->onDelete('cascade');
+            $table->foreignId('TariefId')->constrained('tariefs')->onDelete('cascade');
+            $table->foreignId('BaanId')->constrained('baans')->onDelete('cascade');
+            $table->foreignId('PakketOptieId')->constrained('pakket_opties')->onDelete('cascade');
+            $table->foreignId('ReserveringStatusId')->constrained('reservering_statuses')->onDelete('cascade');
+            $table->integer('Reserveringsnummer');
+            $table->date('Datum');
+            $table->tinyInteger('AantalUren');
+            $table->time('BeginTijd');
+            $table->time('EindTijd');
+            $table->integer('AantalVolwassenen');
+            $table->integer('AantalKinderen');
+            $table->boolean('IsActief');
+            $table->string('Opmerking')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reserverings');
+    }
+};
